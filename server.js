dcc70eed5422b5f3c96edcc68db42f3029ef90bf
Wei hou <wei.hou@c2fo.com>
2018-04-25 15:07:24 -0500

    Update to more complex types and queries

--- server.js
+++ server.js
@@ -5,28 +5,53 @@ const { makeExecutableSchema } = require('graphql-tools');
 const HOST = 'localhost';
 const PORT = 3000;
 
-
-// Some fake data
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
+const authors = [
+  { id: 1, firstName: 'Tom', lastName: 'Coleman' },
+  { id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
+  { id: 3, firstName: 'Mikhail', lastName: 'Novikov' },
 ];
 
-// The GraphQL schema in string form
+const posts = [
+  { id: 1, authorId: 1, title: 'Introduction to GraphQL', votes: 2 },
+  { id: 2, authorId: 2, title: 'Welcome to Apollo', votes: 3 },
+  { id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1 },
+  { id: 4, authorId: 3, title: 'Launchpad is Cool', votes: 7 },
+];
+
 const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
+  type Author {
+    id: Int!
+    firstName: String
+    lastName: String
+    posts: [Post] # the list of Posts by this author
+  }
+
+  type Post {
+    id: Int!
+    title: String
+    author: Author
+    votes: Int
+  }
+
+  # the schema allows the following query:
+  type Query {
+    posts: [Post]
+    author(id: Int!): Author
+  }
 `;
 
-// The resolvers
+
 const resolvers = {
-  Query: { books: () => books },
+  Query: {
+    posts: () => posts,
+    author: (_, { id }) => authors.find(author => author.id === id),
+  },
+  Author: {
+    posts: (author) => posts.filter(post => post.authorId === author.id),
+  },
+  Post: {
+    author: (post) => authors.find(author => author.id === post.authorId),
+  },
 };
 
 
